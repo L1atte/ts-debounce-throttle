a0e8cfa7a8675d317cc1a4de5a363195ac7c3eb5
L1atte <l4atte@gmail.com>
2023-04-20 01:01:22 +0800

    build: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
+      name: "ts-debounce-throttle",
+      fileName: "index",
       entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es", "cjs"],
+      formats: ["es", "cjs", "umd"],
     },
   },
+  plugins: [dts()],
 });
